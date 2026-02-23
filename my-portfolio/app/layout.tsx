588ee7c8e8ca78ec79bb1aa793f6e8c8e69c5940
oBAllOlo <sittipongj74@gmail.com>
2026-02-23 18:48:45 +0700

    feat: Initialize Next.js root layout with global styles, metadata, and core structural components.

--- my-portfolio/app/layout.tsx
+++ my-portfolio/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className="dark" suppressHydrationWarning>
       <body
         className={`${poppins.className} antialiased bg-gray-100 dark:bg-gray-900 text-gray-800 dark:text-gray-100 transition-colors duration-300`}
       >
